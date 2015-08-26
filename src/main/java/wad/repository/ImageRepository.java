a0858717db44122eecc52081c467f45947ab340a
Sami Hämäläinen <sami.einari@gmail.com>
2015-08-26 04:42:50 +0300

    testing avatar fix

--- src/main/java/wad/repository/ImageRepository.java
+++ src/main/java/wad/repository/ImageRepository.java
@@ -3,7 +3,5 @@ package wad.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import wad.domain.AvatarImage;
 
-
 public interface ImageRepository extends JpaRepository<AvatarImage, Long> {
-
 }
