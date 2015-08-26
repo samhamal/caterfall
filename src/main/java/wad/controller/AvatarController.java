56a218e8af34e002a3c39b159417d3e2bd0a245e
Sami Hämäläinen <sami.einari@gmail.com>
2015-08-26 04:42:50 +0300

    testing avatar fix

--- src/main/java/wad/controller/AvatarController.java
+++ src/main/java/wad/controller/AvatarController.java
@@ -8,6 +8,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -21,6 +22,7 @@ import wad.service.PersonService;
 
 // Oma feature: Käyttäjät voivat asettaa itselleen kuvan
 @Controller
+@Transactional
 @RequestMapping("/avatar")
 public class AvatarController {
     @Autowired
